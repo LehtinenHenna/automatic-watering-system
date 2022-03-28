9c4558701f322a12fd8f64b5e313a04ff49e2e54
Joonas Pietilä <joonas.pietila@edu.turkuamk.fi>
2022-03-28 20:31:09 +0300

    #13 Added initial Django project and app

---
+++ django/water_world/urls.py
@@ -0,0 +1,12 @@
+"""Determine URLs for Water World app."""
+
+from django.urls import path
+
+from . import views
+
+# Namespace to be used in the views templates
+app_name = "water_world"
+
+urlpatterns = [
+
+]
