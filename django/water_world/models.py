384940f2f5b4176cf2db086851586cf236ef301c
Joonas Pietilä <joonas.pietila@edu.turkuamk.fi>
2022-04-08 14:41:01 +0300

    Initial models for config and water pump

--- django/water_world/models.py
+++ django/water_world/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Config(models.Model):
+  enable_system = models.BooleanField("System on/off switch",)
+  liters_to_pump = models.FloatField("Liters pumped per pump activation",)
+  sensor_read_interval_hours = models.IntegerField("Sensor read interval in hours",)
+
+
+class WaterPump(models.Model):
+  pump_activated = models.DateTimeField("Pump activation timestamp", blank=True, null=True,)
+  pump_stopped = models.DateTimeField("Pump stopping timestamp", blank=True, null=True,)
