7cdbdf385c291c7e8c3a079c68628cef642287df
Henna Lehtinen <henna.lehtinen@edu.turkuamk.fi>
2022-04-09 15:35:14 +0300

    Added database model Event to log events that happen in the app.

--- django/water_world/models.py
+++ django/water_world/models.py
@@ -9,3 +9,9 @@ class Config(models.Model):
 class WaterPump(models.Model):
   pump_activated = models.DateTimeField("Pump activation timestamp", blank=True, null=True,)
   pump_stopped = models.DateTimeField("Pump stopping timestamp", blank=True, null=True,)
+
+
+class Event(models.Model):
+  message = models.TextField("Event message",)
+  event_type = models.CharField("Event type", max_length=100)
+  event_time = models.DateTimeField("When the event happened",)
