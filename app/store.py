4c45aecc780aa42bbfe2112c92a8b3498ceb52c8
Henna Lehtinen <henna.lehtinen@edu.turkuamk.fi>
2022-04-09 18:57:44 +0300

    Fixed documentation. Made functions create_insert_query and create_fetch_all_query. For postgresql connection user and password are now fetched from .env file. Event data is now being logged to the database to table water_world_event. Made the initial functionality of writing data point to database and making a fetch query to database, but they are untested.

--- app/store.py
+++ app/store.py
@@ -3,19 +3,13 @@ A program that receives messages from MQTT broker and writes data and makes quer
 A query should be published to topic <hostname>/database/query and the payload should be a json
 that contains the query as a string and variables that replace the %s instances as a list, e.g.
 { 
-    "query": 
-        "SELECT *
-        FROM %s
-        ORDER BY timestamp DESC 
-        LIMIT 1", 
-    "variables": 
-        ["configuration"]
+    "table": "water_world_config"
 }
 When writing data to Postgresql it should be published to topic <hostname>/database/write and the payload
 should be a json that contains a query string, a list of columns and a list of values 
 that replace the %s instances in the query, e.g.
 {
-    "query": "INSERT INTO numbers {} VALUES (%s, %s)", 
+    "table": "water_world_waterpump"
     "columns": ["liters_per_minute", "hours_between_sensor_reads"],
     "values": [10, 2]
 }
@@ -34,6 +28,22 @@ from socket import gethostname
 from traceback import format_exc
 from psycopg2 import sql
 
+def create_insert_query(data):
+    query = sql.SQL(
+        'INSERT INTO {table} {columns} VALUES {values}').format(
+            table = sql.Identifier(data['table']), 
+            columns = sql.Identifier(data['columns']),
+            values = sql.Identifier(data['values'])
+        )
+    return query
+
+def create_fetch_all_query(data):
+    query = sql.SQL(
+        'SELECT * FROM {table}').format(
+            table = sql.Identifier(data['table'])
+    )
+    return query
+    
 
 def on_connect(client, userdata, flags, rc):
     global subscribeTopic
@@ -42,68 +52,84 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    global publishTopic
+    global host, USER, PASSWORD
+    try:
+        connection = psycopg2.connect('dbname=postgres user=' + USER + ' password=' + PASSWORD)
+        cursor = connection.cursor()
 
-    connection = psycopg2.connect("dbname=waterworld user=jorma")
-    cursor = connection.cursor()
-    
-    receiveTime=datetime.datetime.now()
-    data = json.loads(msg.payload.decode("utf-8"))
+        data = json.loads(msg.payload.decode("utf-8"))
 
-    if 'write' in msg.topic:
-        try:
-            query = data['query']
+        if 'write' in msg.topic:
 
-            insertion = sql.SQL(
-                "insert into {table} {columns} values {values}").format(
-                    table = sql.Identifier('people'), 
-                    column = sql.Identifier(data['column']).as_string(connection),
-                    values = sql.Identifier(data['values']).as_string(connection)
-                )
+            insert_query = create_insert_query(data)
 
-
-            cursor.execute(insertion)
-            client.publish(topic = publishTopic + '/event/success', payload = 'point written to database: ' + str(insertion))
-
+            cursor.execute(insert_query)
             connection.commit()
-        except:
-            print('Error occurred:', format_exc())
 
-    elif 'query' in msg.topic:
-        try:
-            query = data['query']
-            queryVariables = data['variables']
+            success_event_query = create_insert_query({
+                'table': 'water_world_event',
+                'columns': ['message', 'event_type', 'event_time'],
+                'values': [
+                    'Successfully wrote point to database: ' + str(insert_query),
+                    'Success',
+                    datetime.datetime.now()
+                    ]
+                })
+            cursor.execute(success_event_query)
+            connection.commit()
+            
 
+        elif 'query' in msg.topic:
 
-        except:
-            print('Error occurred:', format_exc())
+            select_query = create_fetch_all_query(data)
+            cursor.execute(select_query)
+            list_of_rows = cursor.fetchall()
 
-    connection.close()
+            client.publish(topic = host + '/store/query', payload = json.dumps(list_of_rows))
 
+        connection.close()
+
+    except OSError as oe:
+        os_error_event_query = create_insert_query({
+                'table': 'water_world_event',
+                'columns': ['message', 'event_type', 'event_time'],
+                'values': [
+                    'OSError occured: ' + format_exc,
+                    'Error',
+                    datetime.datetime.now()
+                    ]
+                })
+        cursor.execute(os_error_event_query)
+        connection.commit()
+        connection.close()
+        raise oe
+
+    except Exception:
+        exception_event_query = create_insert_query({
+                'table': 'water_world_event',
+                'columns': ['message', 'event_type', 'event_time'],
+                'values': [
+                    'Exception occured: ' + format_exc,
+                    'Warning',
+                    datetime.datetime.now()
+                    ]
+                })
+        cursor.execute(exception_event_query)
+        connection.commit()
+        connection.close()
         
 
 if __name__ == '__main__':
-    '''
-    # loading environment variables from .env and fetching MONGODB_URI
+
+    # loading environment variables from .env and fetching db user and password
     load_dotenv()
-    MONGODB_URI = os.getenv('MONGODB_URI')
-
-    
-    # Set up client for MongoDB
-    mongoClient = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
-    try:
-        print('mongoClient server info:', mongoClient.server_info())
-    except Exception:
-        print("Unable to connect to the server.")
-    db = mongoClient.waterworld
-    collection = db.wateringSystem
-    '''
-    
+    USER = os.getenv('USER')
+    PASSWORD = os.getenv('PASSWORD')
 
 
-    # Initialize the MQTT client
-    subscribeTopic = gethostname() + '/database/#'
-    publishTopic = gethostname() + '/store'
+    # Initializing the MQTT client
+    host = gethostname()
+    subscribeTopic = host + '/database/#'
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
