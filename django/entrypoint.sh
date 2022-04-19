80556abb0e4469197f4bedad832de35ca1e1e960
Joonas Pietilä <joonas.pietila@edu.turkuamk.fi>
2022-04-19 21:40:09 +0300

    Removed the migrations from entrypoing file

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createcachetable
-
 if [ "$DJANGO_SUPERUSER_USERNAME" ]
 then
   python manage.py createsuperuser \
