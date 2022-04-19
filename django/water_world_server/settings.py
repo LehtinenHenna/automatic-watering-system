dad10e6df939b341a3e506db4b9ad2f6c86cd294
Joonas Pietilä <joonas.pietila@gmail.com>
2022-04-19 20:27:01 +0300

    Added entrypoint script and rest of mongodb config.

--- django/water_world_server/settings.py
+++ django/water_world_server/settings.py
@@ -80,15 +80,11 @@ WSGI_APPLICATION = 'water_world_server.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
-}
+     'default': {
+         'ENGINE': 'djongo',
+         'NAME': 'waterworld',
+     }
+ }
 
 
 # Password validation
