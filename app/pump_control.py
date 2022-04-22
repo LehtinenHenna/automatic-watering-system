214b1a8520bed04547283af7aa7dda960a5ede18
Jorma <emblinux420@gmail.com>
2022-04-22 20:04:23 +0300

    Fixed import errors. Moved water pump time stamp creations and publish to activate_pump. Added except clauses to functions to log events and to turn of relays. Added lines to set relays off to the start of main function. Fixed timestamp issues. Added longer sleep to main loop to wait longer for the data to arrive from the database.

--- app/pump_control.py
+++ app/pump_control.py
@@ -11,11 +11,12 @@ Uses store.py to communicate with the database.
 
 import signal
 import RPi.GPIO as GPIO
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
 import paho.mqtt.client as mqtt
 from socket import gethostname
 import json
+from traceback import format_exc
 
 
 def setup_pins():
@@ -28,87 +29,146 @@ def setup_pins():
 
 def read_sensor(seconds_to_pump):
 	global client
-	GPIO.output(27, 1) # set sensor vcc on
-	sleep(1)
-	moisture = GPIO.input(17)
-	print(f"Moisture: {moisture}")
-	if moisture == 0:
-		print("ITS MOIST BABY")
-	elif moisture == 1:
-		print("DRY AS A DESERT")
-		pump_activated = datetime.now()
+	try:
+		GPIO.output(27, 1) # set sensor vcc on
+		sleep(1)
+		moisture = GPIO.input(17)
+		print(f"Moisture: {moisture}")
+		if moisture == 0:
+			print("ITS MOIST BABY")
+		elif moisture == 1:
+			print("DRY AS A DESERT")
+			activate_pump(seconds_to_pump)
+		GPIO.output(27, 0) # set sensor vcc off
+
+	except:
+		client.publish(topic=host+'/database/write', payload=
+			json.dumps({
+			"table": "water_world_event",
+			"insert_dict": {
+				"message": "pump control failed at read_sensor: " + format_exc(),
+				"event_type": "Warning",
+				"event_time": datetime.now().isoformat()
+		}}))
+		GPIO.output(20, 1) # set relays off
+		GPIO.output(26, 1)
+
+
+def activate_pump(seconds_to_pump):
+	global client
+	try:
+
+		GPIO.output(20, 0) # set relays on
+		GPIO.output(26, 0)
+		pump_activated = datetime.now().isoformat()
 		print(f"Pump started: {pump_activated}")
-		activate_pump(seconds_to_pump)
-		pump_stopped = datetime.now()
+		print("WATEEEEEEEEEEEERRRR")
+		sleep(seconds_to_pump)
+		GPIO.output(20, 1) # set relays off
+		GPIO.output(26, 1)
+		pump_stopped = datetime.now().isoformat()
 		print(f"Pump stopped: {pump_stopped}")
-		client.publish(topic='', payload=
+
+		client.publish(topic=host+'/database/write', payload=
 			json.dumps({
 			"table": "water_world_waterpump",
 			"insert_dict": {
 				"pump_activated": pump_activated,
 				"pump_stopped": pump_stopped,
-			}}))
-	GPIO.output(27, 0) # set sensor vcc off
-
-
-def activate_pump(seconds_to_pump):
-	GPIO.output(20, 0) # set relays on
-	GPIO.output(26, 0)
-	print("WATEEEEEEEEEEEERRRR")
-	sleep(seconds_to_pump)
-	GPIO.output(20, 1) # set relays off
-	GPIO.output(26, 1)
-	
+		}}))
+	except:
+		client.publish(topic=host+'/database/write', payload=
+			json.dumps({
+			"table": "water_world_event",
+			"insert_dict": {
+				"message": "pump control failed at activate_pump: " + format_exc(),
+				"event_type": "Warning",
+				"event_time": datetime.now().isoformat()
+		}}))
+		GPIO.output(20, 1) # set relays off
+		GPIO.output(26, 1)
+		
 
 def main():
-	global host, client, enable_system, system_mode, seconds_to_pump, sensor_read_interval_hours, watering_interval_hours, config_updated
-	setup_pins()
-	sensor_mode = 1
-	timer_mode = 2
-	# initializing watering time to 72h from this moment and sensor reading to 1h from this moment
-	watering_timestamp = datetime.datetime.now() + datetime.timedelta(hours=72)
-	sensor_reading_timestamp = datetime.datetime.now() + datetime.timedelta(hours=1)
-	previous_watering_interval = 0
-	previous_sensor_interval = 0
+	try:
+		global host, client, enable_system, system_mode, seconds_to_pump, sensor_read_interval_hours, watering_interval_hours, config_updated
+		setup_pins()
+		GPIO.output(20, 1) # set relays off
+		GPIO.output(26, 1)
 
-	while True:
-		# make a query to get data from table water_world_config
-		client.publish(topic=host+'/database/query', payload=json.dumps({"table": "water_world_config"}))
-		sleep(1)
-		if config_updated == True: # if the query data came through
-			if enable_system == True: # if system is turned on
+		sensor_mode = 1
+		timer_mode = 2
+		# initializing watering time to 72h from this moment and sensor reading to 1h from this moment
+		watering_timestamp = datetime.now() + timedelta(hours=72)
+		sensor_reading_timestamp = datetime.now() + timedelta(hours=1)
+		previous_watering_interval = 0
+		previous_sensor_interval = 0
 
-				if system_mode == sensor_mode: # if watering happens according to sensor reading
-					if previous_sensor_interval != sensor_read_interval_hours:
-						# update sensor_reading_timestamp
-						time = datetime.datetime.now()
-						sensor_reading_timestamp = time + datetime.timedelta(hours=sensor_read_interval_hours)
-					if datetime.datetime.now() >= sensor_reading_timestamp:
-						# it's time to read the sensor
-						read_sensor(seconds_to_pump)
-					
-				elif system_mode == timer_mode: # if watering happens according to time interval
-					if previous_watering_interval != watering_interval_hours:
-						# update watering_timestamp
-						time = datetime.datetime.now()
-						watering_timestamp = time + datetime.timedelta(hours=watering_interval_hours)
-					if datetime.datetime.now() >= watering_timestamp: 
-						# it's watering time
-						activate_pump(seconds_to_pump)
+		while True:
+			# make a query to get data from table water_world_config and wait for the results
+			client.publish(topic=host+'/database/query', payload=json.dumps({"table": "water_world_config"}))
+			sleep(3)
 
-			else: # system is turned off
-				# publish success event that system is off and sensor wasn't read
-				client.publish(topic=host+'/database/write', payload=
-					json.dumps({
-					"table": "water_world_event",
-					"insert_dict": {
-						"message": "System is turned off",
-						"event_type": "Success",
-						"event_time": datetime.now()
-					}}))
-			config_updated = False
-			sleep(600) # sleep 10 min
+			if config_updated == True: # if the query data came through
+				print('config_updated true')
+				if enable_system == True: # if system is turned on
+					print('enable_system true')
+					if system_mode == sensor_mode: # if watering happens according to sensor reading
+						print('system_mode == sensor_mode')
+						if previous_sensor_interval != sensor_read_interval_hours:
+							# set new future moment for sensor reading
+							time = datetime.now()
+							sensor_reading_timestamp = time + timedelta(hours=sensor_read_interval_hours)
+							previous_sensor_interval = sensor_read_interval_hours
+						if datetime.now() >= sensor_reading_timestamp:
+							print("it's time to read the sensor")
+							# it's time to read the sensor
+							read_sensor(seconds_to_pump)
+							# set new future moment for sensor reading
+							time = datetime.now()
+							sensor_reading_timestamp = time + timedelta(hours=sensor_read_interval_hours)
+						print('sensor_reading_timestamp', sensor_reading_timestamp)
+						
+					elif system_mode == timer_mode: # if watering happens according to time interval
+						print('system_mode == timer_mode')
+						if previous_watering_interval != watering_interval_hours:
+							# set new future moment for watering
+							time = datetime.now()
+							watering_timestamp = time + timedelta(hours=watering_interval_hours)
+							previous_watering_interval = watering_interval_hours
+						if datetime.now() >= watering_timestamp:
+							print("it's watering time") 
+							# it's watering time
+							activate_pump(seconds_to_pump)
+							# set new future moment for watering
+							time = datetime.now()
+							watering_timestamp = time + timedelta(hours=sensor_read_interval_hours)
+						print('watering_timestamp', watering_timestamp)
 
+				else: # system is turned off
+					# publish success event that system is off and sensor wasn't read
+					client.publish(topic=host+'/database/write', payload=
+						json.dumps({
+						"table": "water_world_event",
+						"insert_dict": {
+							"message": "System is turned off",
+							"event_type": "Success",
+							"event_time": datetime.now().isoformat()
+						}}))
+				config_updated = False
+				sleep(10) # sleep 10 sec
+				print('***AT THE END OF THE LOOP***')
+	except:
+		client.publish(topic=host+'/database/write', payload=
+			json.dumps({
+			"table": "water_world_event",
+			"insert_dict": {
+				"message": "pump control failed at main: " + format_exc(),
+				"event_type": "Warning",
+				"event_time": datetime.now().isoformat()
+		}}))
+		GPIO.output(20, 1) # set relays off
+		GPIO.output(26, 1)
 
 def on_connect(client, userdata, flags, rc):
 	global subscribeTopic
@@ -117,21 +177,34 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-	global enable_system, seconds_to_pump, sensor_read_interval_hours, enable_sensor, watering_interval_hours, config_updated
-	# received data from database
-	data = json.loads(msg.payload.decode())
-	print('data should be a dict:', data)
+	global enable_system, system_mode, seconds_to_pump, sensor_read_interval_hours, enable_sensor, watering_interval_hours, config_updated
+	try:
+		# received data from database
+		data = json.loads(msg.payload.decode())
+		print('data:', data)
 
-	enable_system = data['enable_system']
-	system_mode = data['system_mode']
-	seconds_to_pump = data['seconds_to_pump']
-	sensor_read_interval_hours = data['sensor_read_interval_hours']
-	watering_interval_hours = data['watering_interval_hours']
+		enable_system = data['enable_system']
+		system_mode = data['system_mode']
+		seconds_to_pump = data['seconds_to_pump']
+		sensor_read_interval_hours = data['sensor_read_interval_hours']
+		watering_interval_hours = data['watering_interval_hours']
 
-	config_updated = True
+		config_updated = True
+	except:
+		client.publish(topic=host+'/database/write', payload=
+			json.dumps({
+			"table": "water_world_event",
+			"insert_dict": {
+				"message": "pump control failed at on_message: " + format_exc(),
+				"event_type": "Warning",
+				"event_time": datetime.now().isoformat()
+		}}))
+		GPIO.output(20, 1) # set relays off
+		GPIO.output(26, 1)
 
 
 if __name__ == "__main__":
+
 	# initializing the MQTT client
 	host = gethostname()
 	print('host: ', host)
