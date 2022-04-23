4e92f39acdf346e71181c3af40d1fff56effea3c
Joonas Pietilä <joonas.pietila@edu.turkuamk.fi>
2022-04-23 14:17:07 +0300

    Added very basic template template views and url routing for events and pump actions.

--- django/water_world/views.py
+++ django/water_world/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Config
+from .models import Config, Event, WaterPump
 
 
 class ConfigView(generic.base.View):
@@ -13,3 +13,23 @@ class ConfigView(generic.base.View):
         Return the last config.
         """
         return Config.objects.all()[0]
+
+
+class EventView(generic.base.TemplateView):
+    template_name = "water_world/events.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_events'] = Event.objects.all()[:30]
+        
+        return context
+
+
+class PumpView(generic.base.TemplateView):
+    template_name = "water_world/pump.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_actions'] = WaterPump.objects.all()[:30]
+        
+        return context
