cec0c0ce2eae285b720b97c02c94d041df5ce685
Joonas Pietilä <joonas.pietila@edu.turkuamk.fi>
2022-04-23 14:17:07 +0300

    Added very basic template template views and url routing for events and pump actions.

--- django/water_world/urls.py
+++ django/water_world/urls.py
@@ -9,4 +9,6 @@ app_name = "water_world"
 
 urlpatterns = [
     path("config/", views.ConfigView.as_view(), name="config"),
+    path('events/', views.EventView.as_view(), name='events'),
+    path('pump/', views.PumpView.as_view(), name='pump'),
 ]
